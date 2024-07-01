a6c1a89c83f02322c51b8a8628f868c5d029addf
Praveen Soni <praveensoni9291@gmail.com>
2024-07-01 17:10:03 +0530

    second commit

--- frontend/index.js
+++ frontend/index.js
@@ -9,18 +9,69 @@ searchform.addEventListener('submit', fetchData);
 let url= `http://localhost:3000/review`
 
 
+
+function printFetchedData(response){
+
+    let arrayOfCompany= response.data;
+
+    let resultDiv= document.getElementById('resultDiv');
+    
+    let sumOfRating= 0;
+    arrayOfCompany.forEach(element => {
+        sumOfRating+=element.rating;
+    });
+
+    let averageRating = sumOfRating/ arrayOfCompany.length*1;
+
+
+
+    let companyDetail = `
+    <div style="margin-top: 4rem;">
+        <h3 style="text-decoration: underline; margin-bottom: 3rem">Company Reviews</h3>
+        <h4>Company Name: <span>${arrayOfCompany[0].companyName}</span></h4>
+        <h4>Company Rating: <span>${averageRating}</span></h4>
+    </div>`;
+
+
+
+
+    resultDiv.innerHTML= companyDetail;
+
+    arrayOfCompany.forEach(element => {
+        let reviewsList = `
+            <div style="margin-top: 1rem;">
+                <h4>________________________________</h4>
+                <h6>Pros: <span>${element.pros}</span></h6>
+                <h6>Cons: <span>${element.cons}</span></h6>
+                <h6>Star Rating: <span>${element.rating}</span></h6>
+            </div>`;
+        resultDiv.innerHTML += reviewsList;
+
+
+   
+
+
+    })
+}
+
+
 async function fetchData(e){
     e.preventDefault();
 
     let searchCompany= document.getElementById('searchCompany').value;
+    searchform.reset();
     
     try {
         let result= await axios.get(`${url}?companyName=${searchCompany}`)
-
         console.log(result.data);
+
+        printFetchedData(result);
+
+        
 
         
     } catch (error) {
+        console.log("error in printing");
         
     }
 
